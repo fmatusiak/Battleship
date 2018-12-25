27780e36366a6876758c4cacbbc843fefab9ff56
fmatusiak <f.matusiak95@gmail.com>
2018-12-25 16:39:11 +0100

    add  method updateCountShipsPlayer to class Result

--- src/main/java/battleship/result/Result.java
+++ src/main/java/battleship/result/Result.java
@@ -1,5 +1,28 @@
 package battleship.result;
 
-public class Result {
+import battleship.ship.ListShips;
 
+public class Result {
+    ListShips listShips;
+
+    int shipsPlayer = 0;
+    int shipsComputer = 0;
+
+    public Result(ListShips listShips) {
+        this.listShips = listShips;
+    }
+
+    public boolean updateCountShipsPlayer() {
+        shipsPlayer = listShips.getPlayerListShips().size();
+        shipsComputer = listShips.getComputerListShips().size();
+        return true;
+    }
+
+    public int getShipsPlayer() {
+        return shipsPlayer;
+    }
+
+    public int getShipsComputer() {
+        return shipsComputer;
+    }
 }
